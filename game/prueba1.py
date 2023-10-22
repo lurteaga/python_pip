061192611a306b2148bbf313c5d6b63f878e586b
lurteagat <lurteagat@uni.pe>
2023-10-22 12:48:20 -0500

    probar un juego

---
+++ game/prueba1.py
@@ -0,0 +1,93 @@
+import random
+
+
+def choose_options():
+  options = ('piedra', 'papel', 'tijera')
+  user_option = input('piedra, papel o tijera => ')
+  user_option = user_option.lower()
+
+  if not user_option in options:
+    print('esa opcion no es valida')
+    # continue
+    return None, None
+
+  computer_option = random.choice(options)
+
+  print('User option =>', user_option)
+  print('Computer option =>', computer_option)
+  return user_option, computer_option
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
+  if user_option == computer_option:
+    print('Empate!')
+  elif user_option == 'piedra':
+    if computer_option == 'tijera':
+      print('piedra gana a tijera')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('Papel gana a piedra')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'papel':
+    if computer_option == 'piedra':
+      print('papel gana a piedra')
+      print('user gano')
+      user_wins += 1
+    else:
+      print('tijera gana a papel')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'tijera':
+    if computer_option == 'papel':
+      print('tijera gana a papel')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('piedra gana a tijera')
+      print('computer gano!')
+      computer_wins += 1
+  return user_wins, computer_wins
+
+def run_game():
+  computer_wins = 0
+  user_wins = 0  
+  rounds = 1
+  while True:
+    print('*' * 10)
+    print('ROUND', rounds)
+    print('*' * 10)
+
+    print('computer_wins', computer_wins)
+    print('user_wins', user_wins)
+    rounds += 1
+
+    user_option, computer_option = choose_options()
+    user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+    #código para que el primero que llegue a 2 gane
+
+    if computer_wins == 2:
+      print('El ganador es la computadora')
+      break
+
+    if user_wins == 2:
+      print('El ganador es el usuario')
+      break
+
+run_game()
+
+# ------------------
+# def dividir(a, b):
+#     if b == 0:
+#         print("Error: División por cero.")
+#         return# si no se coloca return aparecera un mensaje de traceback y de ZeroDivisionError
+#     resultado = a / b
+#     print(f"El resultado de la división es: {resultado}")
+
+# # Llamamos a la función
+# dividir(10, 2)  # Esto funciona correctamente
+# dividir(10, 0) 
+# ------------------
+
+
